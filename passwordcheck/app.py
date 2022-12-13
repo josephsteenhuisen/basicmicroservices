d197333e7b112d44cd473cfcb22a36230ed40454
josephsteenhuisen <110561019+josephsteenhuisen@users.noreply.github.com>
2022-12-12 21:29:17 -0500

    Update app.py

--- passwordcheck/app.py
+++ passwordcheck/app.py
@@ -19,18 +19,35 @@ def echo():
 #
 # curl -d "{ \"password\" : \"xxxxxxxx\" }" -X POST http://localhost:9001/check  -H "Content-type: application/json"
 #
+
+#password should be 8 or more characters and have a capital letter and number
+
 @app.route("/check", methods=["POST"])
 def compute():
     hostName = socket.gethostname()
 
     password = request.json['password']
     password_length = len(password)
+    
+    var1 = False
+    var2 = False
+    
+    for i in range(password_length):
+        if(password[i]>='A' and password[i]<='Z'):
+            var1 = True
+    for i in range(10):
+        n = str(i)
+        for j in range(password_length):
+            if (password[j] == n):
+                var2 = True
 
     returnDictionary = {}
     returnDictionary["password"] = password
     returnDictionary["length"] = password_length
+    returnDictionary["contains_number"] = var2
+    returnDictionary["contains_uppercase_letter"] = var1
 
-    if password_length >= 5:
+    if password_length >= 8 and var1 == True and var2 == True:
         returnDictionary["success"] = True
     else:
         returnDictionary["success"] = False    
