dcea434fbbfc12663002bd10e4b007140d0ada0a
josephsteenhuisen <110561019+josephsteenhuisen@users.noreply.github.com>
2022-12-12 21:23:01 -0500

    Update app.py

--- emailcheck/app.py
+++ emailcheck/app.py
@@ -26,12 +26,20 @@ def compute():
 
     email = request.json['email']
     number_of_at_signs = email.count("@")
+    number_of_dot_signs = email.count(".")
+    var1 = False
+    var2 = False
+    if email[0] == "@" or email[-1] == "@": var1 = True
+    if number_of_dot_signs >= 1: var2 = True
 
     returnDictionary = {}
     returnDictionary["email"] = email
     returnDictionary["at_signs"] = number_of_at_signs
+    returnDictionary["at_sign_at_end_or_beginning"] = var1
+    returnDictionary["assert_dot_in_email"] = var2
+    
 
-    if number_of_at_signs == 1:
+    if number_of_at_signs == 1 and var1 == False and var2 == True:
         returnDictionary["success"] = True
     else:
         returnDictionary["success"] = False    
